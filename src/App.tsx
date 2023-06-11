84f5a1f77101258b8432c9213e4a0485a619bb96
Florent <florent.vincerot@me.com>
2023-06-12 01:14:30 +0200

    ok to state vs props

--- src/App.tsx
+++ src/App.tsx
@@ -3,9 +3,13 @@ import ListGroup from "./components/ListGroup";
 function App() {
   let items = ["new-york", "sans francisco", "tokyo", "london", "paris"];
 
+  const handleSelectItem = (item: string) => {
+    console.log(item);
+  }
+
   return (
     <div>
-      <ListGroup items={items} heading={"Cities"} />
+      <ListGroup items={items} heading={"Cities"} onSelectItem={handleSelectItem} />
     </div>
   );
 }
