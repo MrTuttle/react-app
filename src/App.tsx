49cc68a84fa5e2899d6d675102744c2fac4b7f7b
Florent <florent.vincerot@me.com>
2023-06-20 12:17:48 +0200

    go to build a form

--- src/App.tsx
+++ src/App.tsx
@@ -1,22 +1,37 @@
 import { useState } from "react";
+import ListGroup from "./components/ListGroup";
+
 import Button from "./components/Button";
 import Alert from "./components/Alert";
+import Like from './components/Like';
 
 
 function App() {
   // state hook return a array of two states
   const [alertVisible, setAlertVisibility] = useState(false);
+  let items = ["new-york", "sans francisco", "tokyo", "london", "paris"];
+  const handleSelectItem = (item: string) => {
+    console.log(item);
+  }
   return (
-    <div>
-      // condition && : rendre l'alerte uniquement si elle est définit sur true
-      { alertVisible && <Alert onClose={() => setAlertVisibility(false)}>Mon alerte</Alert>}
-      {console.log(`alertVisible = ${alertVisible}`)}
-      {console.log(`setAlertVisibility = ${setAlertVisibility}`)}
-      {console.log(`useState = ${useState}`)}
+    <>
+      <div>
+        <Like onClick={ (toggle) => console.log('clicked') }/>
+
+      </div>
+      <div>
+        <ListGroup items={items} heading={"Cities"} onSelectItem={handleSelectItem} />
+      </div>
+      <div>
+        { alertVisible && <Alert onClose={() => setAlertVisibility(false)}>Mon alerte</Alert>}
+        {console.log(`alertVisible = ${alertVisible}`)}
+        {console.log(`setAlertVisibility = ${setAlertVisibility}`)}
+        {console.log(`useState = ${useState}`)}
 
 
-      <Button onClick={() => setAlertVisibility(true)}>Mon bouton</Button>
-    </div>
+        <Button onClick={() => setAlertVisibility(true)}>Mon bouton</Button>
+      </div>
+    </>
   )
 }
 
---
+++ src/components/Like.tsx
@@ -0,0 +1,19 @@
+import { useState } from "react";
+import { AiOutlineHeart, AiFillHeart } from "react-icons/ai";
+
+interface Props {
+  onClick: () => void;
+
+}
+
+const Like = ({onClick}: Props) => {
+  const [status, setStatus] = useState(true);
+  const toggle = () => {
+    setStatus(!status);
+    onClick();
+  }
+  if (status) return <AiOutlineHeart color="red" size="2rem" onClick={toggle}/>;
+  return <AiFillHeart size="2rem" onClick={toggle}/>;
+};
+
+export default Like;
