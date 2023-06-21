684c83ccdd7638d23ef28fad1851232cbc1d9d05
Florent <florent.vincerot@me.com>
2023-06-21 17:54:52 +0200

    spread operator

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,30 @@
-import React from 'react'
-import Form from './components/form'
+import { useState } from "react";
 
-function App() {
+function App () {
+  const [drink, setDrink] = useState({
+    title: "Americano",
+    price: 5,
+  });
+
+  const handleClick = () => {
+    const newDrink = {
+      // copy the title
+      title: drink.title,
+      // change the price to 6
+      price: 6
+    }
+    // pass newDrink to the set drink function
+    setDrink(newDrink);
+    console.log(handleClick);
+
+  };
+
   return (
     <div>
-      <Form />
+      {drink.price}
+      <button onClick={handleClick}>Click me</button>
     </div>
   );
 }
 
-export default App
+export default App;
