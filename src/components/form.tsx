d7fbd34bea21df2619f933db8d5d9e260dbed366
Florent <florent.vincerot@me.com>
2023-06-21 10:23:42 +0200

    validation forms ok, go to manage validation with zod

--- src/components/form.tsx
+++ src/components/form.tsx
@@ -1,40 +1,40 @@
-import { FormEvent, useState } from "react";
+import { FieldValues, useForm } from "react-hook-form";
+
+interface FormData{
+  name: string;
+  age: number;
+}
 
 const Form = () => {
-  const [person, setPerson] = useState({
-    name: "",
-    age: "",
-  });
+  const {register, handleSubmit, formState: { errors }} = useForm<FormData>();
 
-  const handleSubmit = (event: FormEvent) => {
-    event.preventDefault();
-    console.log(person);
-  };
+  const onSubmit = (data: FieldValues) => console.log(data);
+
+
+
+
   return (
-    <form onSubmit={handleSubmit}>
+    <form onSubmit={ handleSubmit(onSubmit)}>
       <div className="mb-3">
         <label htmlFor="name" className="form-label">
           Name
         </label>
         <input
-          onChange={(event) =>
-            setPerson({ ...person, name: event.target.value })
-          }
-          value={person.name}
+        {...register('name', {required:true, minLength:3})}
           type="text"
           id="name"
           className="form-control"
         />
+        {errors.name?.type === 'required' && <p className="text-danger"> The name field is required</p>}
+        {errors.age?.type === 'minLength' && <p>The name must have at least 3 characteres</p>}
       </div>
       <div className="mb-3">
         <label htmlFor="age" className="form-label">
           Age
         </label>
         <input
-          onChange={(event) =>
-            setPerson({ ...person, age: parseInt(event.target.value) })
-          }
-          value={person.age}
+          {...register('age')}
+
           type="number"
           id="age"
           className="form-control"
