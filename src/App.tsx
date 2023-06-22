4daa62e1cff2d8d216976d4038c5b18ad9664e8f
Florent <florent.vincerot@me.com>
2023-06-22 13:54:41 +0200

    go to Symplify Update logic with Immer

--- src/App.tsx
+++ src/App.tsx
@@ -1,27 +1,20 @@
 import { useState } from "react";
 
-function App () {
-  const [drink, setDrink] = useState({
-    title: "Americano",
-    price: 5,
-  });
+function App() {
+  const [bugs, setBugs] = useState([
+    {id: 1, title: 'bug 1', fixed: false }
+    {id: 2, title: 'bug 2', fixed: true }
+
+  ])
 
   const handleClick = () => {
-    const newDrink = {
-      // copy the title
-      title: drink.title,
-      // change the price to 6
-      price: 6
-    }
-    // pass newDrink to the set drink function
-    setDrink(newDrink);
-    console.log(handleClick);
+    setBugs(bugs.map(bug => bug.id === 1 ? {...bug, fixed: true} : bug))
+
 
   };
 
   return (
     <div>
-      {drink.price}
       <button onClick={handleClick}>Click me</button>
     </div>
   );
