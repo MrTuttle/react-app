1b6c3c66e50f14c53b63fe0b0fee257d6dda3bcc
Florent <florent.vincerot@me.com>
2023-06-26 13:53:48 +0200

    start category form

--- src/components/FormProducts.tsx
+++ src/components/FormProducts.tsx
@@ -1,20 +1,24 @@
 import { FieldValues, useForm } from "react-hook-form";
-import {z} from 'zod';
-import { zodResolver } from '@hookform/resolvers/zod';
+import { z } from "zod";
+import { zodResolver } from "@hookform/resolvers/zod";
 
 const schema = z.object({
-  description: z.string().min(3, {message: 'le nom doit contenir 3 caractères minimum'}),
-  amount: z.number({invalid_type_error: 'amount field is required'}).min(18, {message: "l'amount doit être supérieur ou égal à 18 ans"})
-})
+  description: z
+    .string()
+    .min(3, { message: "le nom doit contenir 3 caractères minimum" }),
+  amount: z
+    .number({ invalid_type_error: "amount field is required" })
+    .min(18, { message: "l'amount doit être supérieur ou égal à 18 ans" }),
+});
 
-type FormData = z.infer<typeof schema>
+type FormData = z.infer<typeof schema>;
 
 const Form = () => {
   const {
     register,
     handleSubmit,
     formState: { errors, isValid },
-  } = useForm<FormData>({ resolver: zodResolver(schema)});
+  } = useForm<FormData>({ resolver: zodResolver(schema) });
 
   const onSubmit = (data: FieldValues) => console.log(data);
 
@@ -33,14 +37,13 @@ const Form = () => {
         {errors.description && (
           <p className="text-danger">{errors.description.message}</p>
         )}
-
       </div>
       <div className="mb-3">
         <label htmlFor="amount" className="form-label">
           Amount
         </label>
         <input
-          {...register("amount", { valueAsNumber: true})}
+          {...register("amount", { valueAsNumber: true })}
           type="number"
           id="amount"
           className="form-control"
@@ -48,6 +51,15 @@ const Form = () => {
         {errors.amount && (
           <p className="text-danger">{errors.amount.message}</p>
         )}
+      </div>
+      <div className="form-group mb-3">
+        <label htmlFor="" className="form-label"></label>
+        <select type="text" className="form-control" >
+          <option value="">Groceries</option>
+          <option value="">Utilities</option>
+          <option value="">Entertainment</option>
+
+        </select>
       </div>
       <button disabled={!isValid} className="btn btn-primary" type="submit">
         Submit
