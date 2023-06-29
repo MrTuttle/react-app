0873c554b0528c98c7a61435277a0cb15496d1a3
Florent <florent.vincerot@me.com>
2023-06-29 15:42:19 +0200

    add padding

--- src/App.tsx
+++ src/App.tsx
@@ -12,7 +12,9 @@ function App() {
   ]);
   return (
     <div>
-     <ExpenseFilter onSelectCategory={category => console.log(category)}/>
+     <div className="mb-3">
+       <ExpenseFilter onSelectCategory={category => console.log(category)}/>
+     </div>
       <ExpenseList expenses={expenses} onDelete={(id) => setExpenses(expenses.filter(e => e.id !== id))}/>
     </div>
   );
