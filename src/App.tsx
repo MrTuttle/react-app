c5e264f46a24d5fc86f0f90f2dcab2c0dfec2cad
Florent <florent.vincerot@me.com>
2023-07-05 09:38:40 +0200

    connecting to the back end : Understanding the effect hook ok, -> go to Effect Deendencies

--- src/App.tsx
+++ src/App.tsx
@@ -1,28 +1,19 @@
-import { useState } from "react";
+import { useEffect, useRef } from "react";
 
 function App() {
-  const [cart, setCart] = useState({
-    discount: .1,
-    items: [
-      {id: 1, title: 'product 1', quantity: 1},
-      {id: 2, title: 'product 2', quantity: 1},
+  const ref = useRef<HTMLInputElement>(null);
 
-    ]
+
+
+  useEffect(() => {
+    document.title = "My App";
   });
-
-  const handleClick = () => {
-    setCart({...cart, items: cart.items.map( item =>  item.id === 1 ? {...item, quantity: item.quantity + 1} : item)})
-    console.log(cart.items)
-
-
-  }
 
   return (
     <div>
-      <p>Item 1 quantité : </p>
-      <button onClick={handleClick}>Click me</button>
-
+      <input ref={ref} type="text" className="form-control" />
     </div>
   );
 }
+
 export default App;
