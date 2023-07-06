e5733f6a01aa355f2abdd2d3d44d1c1b59d5705b
Florent <florent.vincerot@me.com>
2023-07-06 16:56:21 +0200

    cancell http request

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,6 @@
 // src/App.tsx
 
-import axios from "axios";
+import axios, { CanceledError } from "axios";
 import { useEffect, useState } from "react";
 
 interface User {
@@ -13,11 +13,17 @@ function App() {
   const [error, setError] = useState('');
 
   useEffect(() => {
+    const controller = new AbortController();
     axios
 			// get -> promise -> res / err
-      .get<User[]>("https://jsonplaceholder.typicode.com/xusers")
+      .get<User[]>("https://jsonplaceholder.typicode.com/users", {signal: controller.signal})
       .then(res => setUsers(res.data))
-      .catch(err => setError(err.message));
+      .catch(err => {
+        if (err instanceof CanceledError) return;
+        setError(err.message);
+      });
+
+      return () => controller.abort();
   }, [])
 
   return (
